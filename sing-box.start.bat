42dc5266bd376896cd1f5e66c710d6c0f6bb1432
ewigl <ewiglichtl@gmail.com>
2026-07-13 16:00:06 +0800

    fix: correct path to outbounds.json in scripts and .gitignore

--- sing-box.start.bat
+++ sing-box.start.bat
@@ -2,4 +2,4 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
-sing-box.exe merge config.json -c outs/outbounds.json -C fields && sing-box.exe run
+sing-box.exe merge config.json -c outbounds/outbounds.json -C fields && sing-box.exe run
--- sing-box.start.vbs
+++ sing-box.start.vbs
@@ -1,7 +1,7 @@
 Set WS = CreateObject("WScript.Shell")
 
 Dim errorCode
-errorCode = WS.Run("sing-box.exe merge config.json -c outs/outbounds.json -C fields", 0, True)
+errorCode = WS.Run("sing-box.exe merge config.json -c outbounds/outbounds.json -C fields", 0, True)
 
 If errorCode = 0 Then
     WS.Run "sing-box.exe run", 0
